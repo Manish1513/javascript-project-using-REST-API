da93ca4d287ad7272383655d2b89e024c0532f30
manish1513 <gurjarmanish1513@gmail.com>
2024-01-05 18:39:31 +0530

    Final javascript project using Rest Api

--- script.js
+++ script.js
@@ -1,82 +1,69 @@
-
-function saveToLocalStorage(event) {
+let totalprice=0
+function crud(event){
     event.preventDefault();
-    const sellingPrice = document.getElementById('sellingPrice').value;
-    const productName = document.getElementById('productName').value;
+    let price=event.target.num.value;
+    let name=event.target.name.value;
 
-    const product = {
-        sellingPrice,
-        productName
-    };
-
-    axios.post("https://crudcrud.com/api/ca399d15f2244b7aa6d0be6cb96b2465/productdetails", product)
-        .then((response) => {
-            showNewProductOnScreen(response.data);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-
-    // Clear the form inputs
-    document.getElementById('sellingPrice').value = '';
-    document.getElementById('productName').value = '';
-}
-
-window.addEventListener('DOMContentLoaded', () => {
-    axios.get("https://crudcrud.com/api/ca399d15f2244b7aa6d0be6cb96b2465/productdetails")
-        .then((response) => {
-            for (var i = 0; i < response.data.length; i++) {
-                showNewProductOnScreen(response.data[i]);
-            }
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-});
-
-function showNewProductOnScreen(product) {
-    const parentNode = document.getElementById('ListOfProducts');
-
-    const listItem = document.createElement('li');
-    listItem.textContent = `${product.sellingPrice} - ${product.productName}`;
-
-    const deleteButton = document.createElement('button');
-    deleteButton.textContent = 'Delete';
-    deleteButton.onclick = function () {
-        deleteProduct(product._id);
-    };
-
-    const editButton = document.createElement('button');
-   editButton.textContent = 'Edit';
-    editButton.onclick = function () {
-    editUserDetails(product.sellingPrice, product.productName, product._id);
-};
-    listItem.appendChild(deleteButton);
-    listItem.appendChild(editButton);
-
-    parentNode.appendChild(listItem);
-}
-function editUserDetails(sellingPrice,productName,productId){
-    document.getElementById('sellingPrice').value = sellingPrice;
-    document.getElementById('productName').value = productName;
-
-
-    deleteProduct(productId)
-}
-
-function deleteProduct(productId) {
-    axios.delete(`https://crudcrud.com/api/ca399d15f2244b7aa6d0be6cb96b2465/productdetails/${productId}`)
-        .then((response) => {
-            removeProductFromScreen(response);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-}
-
-function removeProductFromScreen(productId) {
-    const listItemToRemove = document.getElementById(productId);
-    if (listItemToRemove) {
-        listItemToRemove.remove();
+    let obj=
+    {
+        price:price,
+        name:name
     }
+
+    axios.post("https://crudcrud.com/api/07d848d4014c4c8c912ee1b3fdb64215/ecom",obj)
+    .then((res)=>{
+        showuser(res.data)
+
+        totalprice +=Number(price);
+        displayTotalPrice(totalprice);
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+}
+axios.get("https://crudcrud.com/api/07d848d4014c4c8c912ee1b3fdb64215/ecom")
+.then((res)=>{
+    totalprice = res.data.reduce((acc, item) => acc + item.price, 0);
+        displayTotalPrice(totalprice);
+    for(let i=0;i<res.data.length;i++)
+    {
+        showuser(res.data[i]) 
+    }
+})
+.catch((err)=>{
+    console.log(err)
+})
+
+function showuser(obj){
+    let parent=document.getElementById('listItem');
+    let child=document.createElement('li');
+    child.textContent= obj.price+" - "+ obj.name;
+
+    let deletebut=document.createElement('input');
+    deletebut.type="button";
+    deletebut.value="Delete Product"
+
+    child.appendChild(deletebut);
+    parent.appendChild(child)
+
+
+    deletebut.addEventListener('click',deleteuser)
+
+    function deleteuser(){
+        axios.delete(`https://crudcrud.com/api/07d848d4014c4c8c912ee1b3fdb64215/ecom/${obj._id}`)
+        .then(()=>{
+            parent.removeChild(child);
+            totalprice -= obj.price;
+            displayTotalPrice(totalprice);
+        })
+        .catch((err)=>{
+            console.log(err)
+        })
+    }
+
+}
+
+function displayTotalPrice(total) {
+    let totalPriceElement = document.getElementById('TotalCost');
+    totalPriceElement.textContent = "Total Value worth of Products:" + total; 
 }
